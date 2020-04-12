40b3a164f83a71c6962faa0de74b7646d81a5ad7
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2020-04-12 17:48:28 -0300

    testes em python: random, for, orientação a objetos e listas com objetos

--- 5 - RNA/Python/teste.py
+++ 5 - RNA/Python/teste.py
@@ -1,5 +1,49 @@
-import random
+
+def sortear(titulo):
+    print(titulo)
+    import random
+    x = random.random()
+    print("Um número sorteado entre 0 e 1: ",x)
 
 
-x = random.random()
-print(x)
+def repetir(titulo,inicio,fim):
+    print(titulo)
+    for i in range(inicio,fim):
+        print(i)
+
+def orientacao_a_objetos(titulo):
+    print(titulo)
+
+    a = Aluno(100,'Alexandre')
+    print(a.matricula)
+    print(a.nome)
+
+def lista_orientacao_a_objetos(titulo,quantidade):
+    print(titulo)
+
+    lista_alunos = []
+    for i in range(quantidade):
+        matricula = input('Informe matricula: ')
+        nome = input('Informe nome: ')
+        lista_alunos.append(Aluno(matricula,nome))
+
+    for i in lista_alunos:
+        print(i.matricula)
+        print(i.nome)
+        print('===================')
+    
+class Aluno:
+    def __init__(self, matricula, nome):
+        self.matricula = matricula
+        self.nome = nome    
+
+def main():
+    print("Aqui é processo principal")
+    # sortear('Método para testar o random:')
+    # repetir('Método para testar repetição com for',2,7)
+    # orientacao_a_objetos('Método para testar OO:')
+    lista_orientacao_a_objetos('Método para testar lista com objetos:',3)
+
+if __name__ == "__main__":
+    main()
+

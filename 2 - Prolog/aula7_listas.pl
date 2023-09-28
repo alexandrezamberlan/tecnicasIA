90c62123894125c8e538fe53f238f108a223c9be
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2023-09-28 11:20:58 -0300

    Update aula7_listas.pl

--- 2 - Prolog/aula7_listas.pl
+++ 2 - Prolog/aula7_listas.pl
@@ -16,8 +16,33 @@ primeiro([P | _], P).
 ultimo([U], U).
 ultimo([_ | R], U) :- ultimo(R, U).
 
-distancias([450,90,110,55]).
 
+adiciona(N,Lista):- adiciona(N,[],Lista).
+
+adiciona(0,Acumulador,Acumulador).
+adiciona(N,Acumulador,Resultado) :-
+    N > 0,
+    N1 is N - 1,
+    adiciona(N1,[N|Acumulador],Resultado).
+
+idades([ [andre,25], [jose,30] ]).
+casas([casa(1, azul), casa(2, verde)]).
+
+alunos([luiz,paulo,leo,alex]).
+distancias([120,34,100,67,12]).
+
+
+primeiro([P | _], P).
+
+ultimo([U], U).
+ultimo([_ | R], U) :- ultimo(R, U).
+
+disciplinas([ia,ed,po,redes,so,modelagem]).
+
+percorre([U]):- writeln(U).
+percorre([P | R]) :- 
+    writeln(P),
+    percorre(R).
 
 FOCO NESTE ESTUDO
 - senteças ou cláusulas declarativas

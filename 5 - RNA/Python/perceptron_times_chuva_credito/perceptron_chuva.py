e534d1a24be67d12645407eadb488e7bfe8e0485
Alexandre Zamberlan <alexz@ufn.edu.br>
2026-06-12 09:40:24 -0300

    amostras e saidas em csv

--- 5 - RNA/Python/perceptron_times_chuva_credito/perceptron_chuva.py
+++ 5 - RNA/Python/perceptron_times_chuva_credito/perceptron_chuva.py
@@ -62,10 +62,12 @@ class Perceptron:
 			geracoes += 1
 			if aprendeu or geracoes > self.geracoes:
 				print('Quantidade de gerações para aprender: %d\n' % geracoes)
+				print('Pesos: ', self.pesos)
 				break
  
 	## Testes para "novas" amostras
 	def teste(self, amostra):
+		print('Pesos: ', self.pesos)
 		# Inserir o valor do limiar na posição "0" para cada amostra da lista "amostras"
 		amostra.insert(0, self.limiar)
 		# Inicializar potencial de ativação
@@ -79,9 +81,9 @@ class Perceptron:
 		saida_gerada = self.funcao_ativacao_signal(soma)
 
 		if saida_gerada == 1:
-			print('Classe: %d. Time Azul' % saida_gerada)
+			print('Classe: %d. Chuva' % saida_gerada)
 		else:
-			print('Classe: %d. Time Vermelho' % saida_gerada)
+			print('Classe: %d. Sol' % saida_gerada)
  
 	## Função funcao_ativacao_signal
 	def funcao_ativacao_signal(self, soma):
@@ -89,24 +91,10 @@ class Perceptron:
 			return 1
 		return -1
  
-# Amostras (entrada e saída) para treinamento
-amostras = [[0.72, 0.82],   [0.91, -0.69], 
-	[0.46, 0.80],   [0.03, 0.93],
-	[0.12, 0.25],   [0.96, 0.47],
-	[0.8, -0.75],   [0.46, 0.98],
-	[0.66, 0.24],   [0.72, -0.15],
-	[0.35, 0.01],   [-0.16, 0.84],
-	[-0.04, 0.68],  [-0.11, 0.1],
-	[0.31, -0.96],  [0.0, -0.26],
-	[-0.43, -0.65], [0.57, -0.97],
-	[-0.47, -0.03], [-0.72, -0.64],
-	[-0.57, 0.15],  [-0.25, -0.43],
-	[0.47, -0.88],  [-0.12, -0.9],
-	[-0.58, 0.62],  [-0.48, 0.05],
-	[-0.79, -0.92], [-0.42, -0.09],
-	[-0.76, 0.65],  [-0.77, -0.76]]
-
-saidas = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+# Dados de treinamento: amostras e saídas - lidas de arquivo
+amostras = []
+# Saídas: -1 = Não Chove (Sol) | 1 = Chove
+saidas = []
  
 # Chamar classe e fazer treinamento
 rede = Perceptron(amostras, saidas)
@@ -115,10 +103,10 @@ rede.treinar()
 # Execução ou produção
 while (True):
 	# Entrando com amostra para teste
-	x = float(input('Valor para x: '))
-	y = float(input('Valor para y: '))
-	print('Ponto: ', x , ' , ', y)
-	rede.teste([x,y])
+	umidade = float(input('Valor para umidade: '))
+	pressao = float(input('Valor para pressao: '))
+	print('Ponto: ', umidade , ' , ', pressao)
+	rede.teste([umidade,pressao])
 	
 	#Os dados utilizados correspondem a pares de coordenadas (x,y) para classificação de cores: 
 	# 1 é azul e -1 é vermelho. 
